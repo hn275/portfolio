fae1a27baac781f6573a4c07bbb418983cda989b
Hal <haln_01@proton.me>
2022-09-07 22:48:00 -0700

    finished stylings for contact pages

--- src/modules/pages/contact/components/form/SubmitButton.tsx
+++ src/modules/pages/contact/components/form/SubmitButton.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BtnMain } from 'modules/common/components/buttons';
+import { ReactComponent as Send } from '../../assets/send.svg';
+import './Form.scss';
 
 interface SubmitButtonTypes {
   handleSubmit: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 export const SubmitButton: React.FC<SubmitButtonTypes> = ({ handleSubmit }) => {
   return (
-    <div className='contact--submit-buttom'>
+    <div className='contact--submit-button'>
       <BtnMain
         handleClick={handleSubmit}
         btnType='submit'
       >
-        Send
+        <>
+          Send{' '}
+          <span>
+            <Send />
+          </span>
+        </>
       </BtnMain>
     </div>
   );
--- src/modules/pages/contact/index.tsx
+++ src/modules/pages/contact/index.tsx
@@ -1,35 +1,68 @@
 import React from 'react';
 import { ReactComponent as ContactSVG } from './assets/contact.svg';
 import { ReactComponent as WaterMark } from './assets/contact_watermark.svg';
+import { ReactComponent as CodeSandBox } from './assets/codesandbox.svg';
+import { ReactComponent as Github } from './assets/github.svg';
+import { ReactComponent as Email } from './assets/email.svg';
+import { ReactComponent as LinkedIn } from './assets/linkedin.svg';
 import { Form } from './components/form';
 import './styles/Contact.scss';
 
 export const Contact = () => {
   return (
-    <section
-      id='contact'
-      className='flex-center container--layout'
-    >
-      <div className='contact--page-wrapper'>
-        <div className='svg--contact'>
-          <div className='svg--watermark'>
-            <WaterMark />
+    <>
+      <section
+        id='contact'
+        className='flex-center container--layout'
+      >
+        <div className='contact--page-wrapper'>
+          <div className='svg--contact'>
+            <div className='svg--watermark'>
+              <WaterMark />
+            </div>
+            <div className='svg--contact-asset'>
+              <ContactSVG />
+            </div>
           </div>
-          <div className='svg--contact-asset'>
-            <ContactSVG />
+
+          <div className='contact--form-container'>
+            <div className='contact--header'>
+              Say <span className='text-dropshadow'>hello</span>
+            </div>
+
+            <div className='contact--form'>
+              <Form />
+            </div>
           </div>
         </div>
 
-        <div className='contact--form-container'>
-          <div className='contact--header'>
-            Say <span className='text-dropshadow'>hello</span>
-          </div>
-
-          <div className='contact--form'>
-            <Form />
-          </div>
+        <div className='contact--external-links'>
+          <a
+            href=''
+            target='_blank'
+          >
+            <Email />
+          </a>
+          <a
+            href=''
+            target='_blank'
+          >
+            <Github />
+          </a>
+          <a
+            href=''
+            target='_blank'
+          >
+            <CodeSandBox />
+          </a>
+          <a
+            href=''
+            target='_blank'
+          >
+            <LinkedIn />
+          </a>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
