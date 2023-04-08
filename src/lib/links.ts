0de9247fa7eebd34ae9fa9802ed74b133add4ea0
Hal <haln_01@proton.me>
2023-04-07 21:31:35 -0700

    feat: links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -2,4 +2,5 @@ export enum LINKS {
   email = "haln_01@proton.me",
   linkedin = "https://www.linkedin.com/in/hal-nguyen-48a22b22a/",
   github = "https://github.com/hn275",
+  collabify = "https://collabify.space",
 }
