f28ddc38fe1d6fca61e9af5606524b2e6f00d171
Hal <haln_01@proton.me>
2023-11-13 22:24:13 -0800

    feat: about page

---
+++ src/assets/links.ts
@@ -0,0 +1,9 @@
+export type SocialLink = {
+    icon: string;
+    href: string;
+};
+export const links: SocialLink[] = [
+    { icon: "cib:linkedin", href: "https://www.linkedin.com/in/hn275/" },
+    { icon: "cib:github", href: "https://github.com/hn275" },
+    { icon: "cib:mail-ru", href: "mailto:haln_01@proton.me" },
+];
