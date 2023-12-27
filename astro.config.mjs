7c2a7c879c7e07962e8e381cd6d92650326160e0
Hal <haln_01@proton.me>
2023-12-26 17:09:09 -0800

    fix: removed adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), svelte()],
-    output: "hybrid",
-    adapter: cloudflare({ mode: "directory" }),
+    // output: "hybrid",
+    // adapter: cloudflare({ mode: "directory" }),
 });
