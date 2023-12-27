b23285861f5b0d433951469ff63d4ee5f2837433
Hal <haln_01@proton.me>
2023-12-26 16:59:36 -0800

    chore: merge with main

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), svelte()],
-    output: "hybrid",
-    adapter: cloudflare(),
+  integrations: [tailwind(), svelte()],
+  output: "hybrid",
+  adapter: cloudflare({ mode: "directory" }),
 });
