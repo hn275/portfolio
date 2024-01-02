5307eea2e9445ea947fa41fabf931ac1bb1a2e7a
Hal <haln_01@proton.me>
2024-01-02 15:31:14 -0800

    feat: form ui toaster

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+    import { afterUpdate } from "svelte";
+
     let email: string;
-    let message: string ;
+    let message: string;
     let name: string;
     let loading = false;
+    let ok = false;
+    let error: string | undefined;
+
     async function handleSubmit() {
         try {
             loading = true;
@@ -22,12 +27,26 @@
             name = "";
             message = "";
             email = "";
+            ok = true;
         } catch (e) {
+            error = "Something went wrong, try again later.";
             console.error(e);
         } finally {
             loading = false;
         }
     }
+
+    afterUpdate(() => {
+        if (ok!) return;
+        const id = setTimeout(() => (ok = false), 5000);
+        return () => clearTimeout(id);
+    });
+
+    afterUpdate(() => {
+        if (!error) return;
+        const id = setTimeout(() => (error = undefined), 5000);
+        return () => clearTimeout(id);
+    });
 </script>
 
 <div class="w-full">
@@ -59,7 +78,7 @@
 
         <button
             type="submit"
-            class="bg-accent-200 text-slate-950 px-3 py-2 rounded-md font-semibold hover:bg-accent-100 transition-all disabled:hover:brightness-100 disabled:bg-slate-800 w-24"
+            class="bg-accent-200 text-slate-950 px-3 py-2 rounded-md font-semibold hover:bg-accent-100 transition-all disabled:hover:brightness-100 disabled:bg-slate-900 disabled:text-late-950 w-24"
             disabled={loading}
         >
             {#if loading}
@@ -71,14 +90,42 @@
     </form>
 </div>
 
+<div class="toast" data-banner={ok}>
+    <span class="bg-green-400">Message sent! Talk to you soon.</span>
+</div>
+
+<div class="toast" data-banner={error !== undefined}>
+    <span class="bg-red-400">{error}</span>
+</div>
+
 <style lang="postcss">
+    .toast[data-banner="true"] {
+        @apply translate-y-0;
+    }
+
+    .toast > span {
+        @apply p-3 rounded-md text-slate-950 w-max font-semibold;
+    }
+
+    .toast {
+        @apply fixed bottom-0 left-0 right-0 p-6 translate-y-full transition-all;
+    }
+
     input,
     textarea {
         @apply bg-slate-900 border border-slate-900;
         @apply min-h-[3em] rounded-md p-3 w-full;
-        @apply placeholder:text-slate-700 placeholder:text-sm;
-        @apply disabled:bg-slate-950 disabled:text-slate-700;
         @apply transition-all;
+    }
+
+    input::placeholder,
+    textarea::placeholder {
+        @apply text-slate-700 text-sm;
+    }
+
+    input:disabled,
+    textarea:disabled {
+        @apply bg-slate-950 text-slate-700;
     }
 
     textarea {
