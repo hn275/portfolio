24af41e376a89ee82dd06370b3bad9fff2263756
Hal <haln_01@proton.me>
2024-01-02 16:24:00 -0800

    chore: added tailwind class sorter:

--- src/assets/links.ts
+++ src/assets/links.ts
@@ -1,10 +1,10 @@
 export type SocialLink = {
-    icon: string;
-    href: string;
+	icon: string;
+	href: string;
 };
 export const email: string = "haln_01@proton.me";
 export const links: SocialLink[] = [
-    { icon: "cib:linkedin", href: "https://www.linkedin.com/in/hn275/" },
-    { icon: "cib:github", href: "https://github.com/hn275" },
-    { icon: "cib:mail-ru", href: `mailto:${email}` },
+	{ icon: "cib:linkedin", href: "https://www.linkedin.com/in/hn275/" },
+	{ icon: "cib:github", href: "https://github.com/hn275" },
+	{ icon: "cib:mail-ru", href: `mailto:${email}` },
 ];
