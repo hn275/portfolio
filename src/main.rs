538b08ac1bbc8c3eba4b4b6f67bc8495af94aa6e
Hal <haln_01@proton.me>
2025-02-11 00:10:41 -0800

    fix for css files

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> io::Result<()> {
     let addr: net::SocketAddr = "0.0.0.0:3000".parse().expect("failed to parse port");
 
     let app = axum::Router::new()
-        .nest_service("/assets", tower_http::services::ServeDir::new("assets"))
+        .nest_service("/assets", tower_http::services::ServeDir::new("./assets"))
         .route("/", axum::routing::get(home::home));
 
     println!("listening at {}", &addr);
