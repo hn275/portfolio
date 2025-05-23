b510d2b8c24f82e123130c4801bc3b6d297def0d
Hal <haln_01@proton.me>
2025-05-23 13:59:22 -0700

    about section done

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -25,17 +25,36 @@ export function Container({
   ...props
 }: {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 }) {
   return (
     <div
       className={clsx(
         "container mx-auto max-w-7xl px-6 flex-grow pt-16",
-        className,
+        className ?? "",
       )}
       {...props}
     >
       {children}
     </div>
   );
+}
+
+export function Section({
+  children,
+  header,
+}: {
+  header: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="px-5 lg:px-0 flex flex-col lg:grid lg:grid-rows-1 lg:grid-cols-[30vw,auto] gap-3 lg:gap-7 justify-center max-w-screen-lg">
+      <h2 className="text-[2em] font-bold lg:justify-self-end">{header}</h2>
+      {children}
+    </div>
+  );
+}
+
+export function SectionBody({ children }: { children: React.ReactNode }) {
+  return <div className="flex flex-col gap-2 lg:max-w-[60ch]">{children}</div>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import { DefaultLayout, Container } from "@/layouts/default";
+import {
+  DefaultLayout,
+  Container,
+  Section,
+  SectionBody,
+} from "@/layouts/default";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
       <section id="#">
-        <Container className="text-[1rem] md:text-[2rem] h-[70vh] grid items-center">
+        <Container className="text-[1rem] md:text-[2rem] h-[90vh] grid items-center">
           <div className="py-10 md:py-20 px-10">
             <h1 className="text-primary font-bold opacity-90 text-[2.5em]">
               hey! my name is hal.
@@ -16,6 +21,34 @@ export default function IndexPage() {
               <p>Cryptography Enthusiast</p>
             </div>
           </div>
+        </Container>
+      </section>
+
+      <section id="about">
+        <Container>
+          <Section header="About">
+            <SectionBody>
+              <span>Hello 👋!</span>
+              <p>
+                I'm in my final year as a Computer Science student at University
+                of Victoria, with a strong focus on Networking and Information
+                Security. Beyond my academic pursuits, I'm deeply passionate
+                about Cryptography, Linux, and Free and Open Source Software.
+              </p>
+              <p>
+                My journey through the tech industry has led me to roles at
+                various startups and freelancing opportunities, where I've
+                authored code as a full-stack software developer. These
+                experiences have introduced me to the modern web technologies,
+                and standard security practices.
+              </p>
+              <p>
+                Motivated to learn, I constantly seek to leverage my skills and
+                expertise to contribute meaningfully to the realms of
+                cybersecurity and open-source innovation.
+              </p>
+            </SectionBody>
+          </Section>
         </Container>
       </section>
     </DefaultLayout>
